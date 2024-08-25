541d5dd5ecb2b19fe94aade0961342c5a8c4cfce
Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-08-25 17:36:34 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ async function checkWeather(city){
         var data = await reaponse.json();
         console.log(data);
 
-        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(". ").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
