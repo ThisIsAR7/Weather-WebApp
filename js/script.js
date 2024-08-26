384e87c6eb2c65997cb518d4142ad1202a25d9d3
Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-08-26 22:54:18 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ async function checkWeather(city){
         } else if(data.weather[0].main == "Drizzle"){
             weatherIcon.src = "css/img/drizzle.webp";
         } else if(data.weather[0].main == "Mist"){
-            weatherIcon.src = "css/img/mist.png";
+            weatherIcon.src = "css/img/mist.";
         }
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
